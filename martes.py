47a9db28de27ad2b511b9e3c1df31e61749aeb12
biangka-jpd <arancibiabianca81@gmail.com>
2025-05-20 16:27:38 -0400

    martes

---
+++ martes.py
@@ -0,0 +1,102 @@
+# cuotaminimaconejos=3
+
+# perros=1,6
+# cuota=3
+
+# for p in range (1, perros + 1):
+#     perros=int(input("cuantos perros quieres ingresar?"))
+#     conejos=int(input("cuantos conejos trajo {perros} ?: "))
+#     cumplio=conejos>=cuota 
+    
+
+
+
+
+# full=0
+# standard=0
+# basico=0
+  
+
+# precios= {
+#      1: 15000, 
+#      2: 10000, 
+#      3: 7000,
+# }
+
+# ventas=[]
+# autos_ingresados=0 
+
+# while True:
+#     print("---MENU LAVADO DE AUTOS---")
+#     print("1- pagar el lavado")
+#     print("2- ver ventas diarias")
+#     print("salir")
+
+#     opcion=input("seleccione la opcion que quiera:")
+
+#     match 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
