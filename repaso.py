e76638b54217c0c2bb5ae39e3738b919d9c79d22
biangka-jpd <arancibiabianca81@gmail.com>
2025-06-03 16:09:43 -0400

    ñaoooooo

--- repaso.py
+++ repaso.py
@@ -27,6 +27,113 @@
 
 
 
+# while True:
+#     try:
+#         opcion=int(input(''' Carrito de compras
+#                         Seleccione una opcion con un numero entero
+#                         1.- Comprar Frutas
+#                         2.- Comprar Verduras
+#                         3.- Pagar
+#                         4.- Salir
+#                             '''))
+#         match opcion:
+#             case 1:
+#                 while True:
+#                     try:
+#                         opcion=int(input('''
+#                                         Seleccione una opcion con un numero entero
+#                                         1.- Frutilla $1500
+#                                         2.- Pera $1200
+#                                         3.- Manzana $ 1300
+#                                         4.- Volver al menu principal
+#                                             '''))
+#                         match opcion:
+#                             case 1:
+#                                 print("Has seleccionado Frutilla")
+#                                 total+=1500
+#                                 cantArt+=1
+#                             case 2:
+#                                 print("Has seleccionado Pera")
+#                                 total+=1200
+#                                 cantArt+=1
+#                             case 3:
+#                                 print("Has seleccionado Manzana")
+#                                 total+=1300
+#                                 cantArt+=1
+#                             case 4:
+#                                 print("Volviendo...")
+#                                 break
+#                             case _:
+#                                 print("Opcion invalida")
+#                     except Exception:
+#                         print("Solo puede ingresar numeros enteros")   
+#                     print(" TU total hasta ahora es ", total)
+#             case 2:
+#                 while True:
+#                     try:
+#                         opcion=int(input('''
+#                                         Seleccione una opcion con un numero entero
+#                                         1.- Papa $1500
+#                                         2.- Lechuga $1200
+#                                         3.- Cebolla $ 1300
+#                                         4.- Volver al menu principal
+#                                             '''))
+#                         match opcion:
+#                             case 1:
+#                                 print("Has seleccionado Papa")
+#                                 cant=int(input("Cuantas papas llevara?"))
+#                                 total+=cant*1500
+#                                 cantArt+=cant
+#                             case 2:
+#                                 print("Has seleccionado Lechuga")
+#                                 total+=1200
+#                                 cantArt+=1
+#                             case 3:
+#                                 print("Has seleccionado Cebolla")
+#                                 total+=1300
+#                                 cantArt+=1
+#                             case 4:
+#                                 print("Volviendo...")
+#                                 break
+#                             case _:
+#                                 print("Opcion invalida")
+#                     except Exception:
+#                         print("Solo puede ingresar numeros enteros")   
+#                     print(" TU total hasta ahora es ", total)
+#             case 3:
+#                 print("Has seleccionado pagar")
+#                 print(f"El total de articulos es {cantArt}")
+#                 print(f"El total a pagar es {total}")
+#                 print(f"El total a pagar mas IVA es {round(total*1.19)}")
+#             case 4:
+#                 print("Saliendo...")
+#                 break
+#             case _:
+#                 print("Opcion invalida")
+#     except Exception:
+#         print("Solo puede ingresar numeros enteros")  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
